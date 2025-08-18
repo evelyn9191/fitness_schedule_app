a1f4a4e7dcdf7c0b97e5b5590504f53f91c96696
Michaela Očková <ockova.michaela@gmail.com>
2025-08-18 19:55:52 +0200

    feat(calendar): Make the deleting script easier to work with

--- delete_events.py
+++ delete_events.py
@@ -12,6 +12,14 @@ def delete_events(query: str | None = None, timeMin: str | None = None, timeMax:
         print(event)
         service.delete_event(event)
 
+
 if __name__ == '__main__':
-    today = datetime.datetime.now(datetime.timezone.utc)
+    now = datetime.datetime.now()
+    two_days_later = now + datetime.timedelta(days=2)
+    time_min = two_days_later.replace(hour=0, minute=0, second=0, microsecond=0)
+    time_max = two_days_later.replace(hour=23, minute=59, second=59, microsecond=999999)
+    time_min_iso = time_min.isoformat() + 'Z'
+    time_max_iso = time_max.isoformat() + 'Z'
+
+    print(f"Deleting events from {time_min} to {time_max}")
     delete_events()
