329199af658b681848b6f1844083b9ab70750db7
Michaela Očková <ockova.michaela@gmail.com>
2025-08-29 16:31:06 +0200

    feat(calendar): Add Origami Yoga

--- run_daily.py
+++ run_daily.py
@@ -3,7 +3,7 @@ import logging
 
 from gcal_updater import GoogleCalendarClient
 from helpers import DATE_FORMAT_CZ
-from schedules import moodyoga, yogakarlin, form_factory
+from schedules import moodyoga, yogakarlin, form_factory, origami_yoga
 
 DAY_TO_TRACK = datetime.date.today() + datetime.timedelta(days=1)
 FROM_TIME = datetime.time(8, 0)
@@ -42,6 +42,7 @@ def get_all_schedules():
         moodyoga.get_schedule,
         yogakarlin.get_schedule,
         form_factory.get_schedule,
+        # origami_yoga.get_schedule,
     ]
     all_schedules = [schedule() for schedule in schedule_functions]
     return sum(all_schedules, [])
